07427d7ad46bdce6539b01dc609ed6df230b3eaa
STREET_OF_HACKER - R007 <51408128+StreetOfHackerR007@users.noreply.github.com>
2020-06-01 17:05:44 +0530

    Add files via upload

--- 404_Digger.py
+++ 404_Digger.py
@@ -2,7 +2,7 @@ import sys
 try:
     import requests
 except:
-    print('[!] requests module not found!\n |-> Try to run "pip install -r requirements.txt" first.')
+    print('[!] requests module not found..!!\n |-> Try to run "pip install -r requirements.txt" first.')
     sys.exit()
 from subprocess import PIPE, Popen
 import platform
@@ -23,55 +23,71 @@ def banner():
 
 
 def check_status(domain):
-    domain = domain.strip('\n')
-    
     try:
-        r = requests.get('http://'+domain, timeout=6)
-        if(r.status_code == 404):
-            statuscode.append(domain)
-            return r.status_code
-    except:
+        domain = domain.strip('\n')
+        
         try:
-            r = requests.get('https://'+domain, timeout=6)
+            r = requests.get('http://'+domain, timeout=6)
             if(r.status_code == 404):
                 statuscode.append(domain)
                 return r.status_code
         except:
-            pass
+            try:
+                r = requests.get('https://'+domain, timeout=6)
+                if(r.status_code == 404):
+                    statuscode.append(domain)
+                    return r.status_code
+            except:
+                pass
+    except KeyboardInterrupt:
+        print('\n[*] CTRL+C detected')
+        sys.exit()
 
 
 def digger():
-    for domain in statuscode:
-        if(platform.system().lower() == "windows"):
-            cmd = '''dig '''+ str(domain) +'''| grep CNAME | awk "{print $1 , $5}" '''
-        else:
-            cmd = '''dig '''+ str(domain) +'''| grep CNAME | awk '{print $1 , $5}' '''
-        output = Popen(args=cmd, stdout=PIPE, shell=True)
-        (output, error) = output.communicate()
+    try:
+        for domain in statuscode:
+            if(platform.system().lower() == "windows"):
+                cmd = '''dig '''+ str(domain) +'''| grep CNAME | awk "{print $1 , $5}" '''
+            else:
+                cmd = '''dig '''+ str(domain) +'''| grep CNAME | awk '{print $1 , $5}' '''
+            output = Popen(args=cmd, stdout=PIPE, shell=True)
+            (output, error) = output.communicate()
 
-        output = output.decode('utf-8')
-    print("    \\")
-    count = 0
-    for cname in output.split():
-        if(count%2 != 0):
-            print('     |-->'+cname)
-        count=count+1
+            output = output.decode('utf-8')
+        print("    \\")
+        count = 0
+        for cname in output.split():
+            if(count%2 != 0):
+                print('     |-->'+cname)
+            count=count+1
+    except KeyboardInterrupt:
+        print('\n[*] CTRL+C detected')
+        sys.exit()
 
 
 if(__name__ == "__main__"):
-    banner()
-    filename = str(input('[+] Enter filename/path : '))
-    statuscode = []
-    
-    with open(filename,'r') as f:
-        domainlst = f.readlines()
+    try:
+        banner()
+        filename = str(input('[+] Enter filename/path : '))
+        statuscode = []
 
-    print('\n\n[*] Please wait while i find 404 response code....')
-    
-    for domain in domainlst:
-        if(check_status(domain) != None):
-            print("\n\n[~] "+str(domain).strip('\n'))
-            digger()
+        try:
+            with open(filename,'r') as f:
+                domainlst = f.readlines()
+        except FileNotFoundError:
+            print('\n[!] '+filename+' File not found..!!\n |-> Check filename and path.')
+            sys.exit()
 
-    if(statuscode == []):
-        print('\n\n[!] 404 Not Found..!!')
+        print('\n\n[*] Please wait while i find 404 response code....')
+        
+        for domain in domainlst:
+            if(check_status(domain) != None):
+                print("\n\n[~] "+str(domain).strip('\n'))
+                digger()
+
+        if(statuscode == []):
+            print('\n\n[!] 404 Not Found..!!')
+    except KeyboardInterrupt:
+        print('\n[*] CTRL+C detected')
+        sys.exit()
